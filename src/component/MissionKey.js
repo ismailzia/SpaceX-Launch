65f38090bc6abeb55d39ac9d0e60e90fc8be8b8e
ziani ismail <ziani0722@gmail.com>
2019-08-29 16:11:02 +0100

    client

---
+++ src/component/MissionKey.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MissionKey() {
+        return (
+            <div className="my-3">
+                <span className='bg-success text-white px-2 mr-2 '>Success</span>
+                <span className='bg-danger text-white px-2'>failure</span>
+            </div>
+        )
+}
+
+export default MissionKey
